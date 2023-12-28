5c413ad142656b6d3fe4e92e748626ff1287132c
Lucian Iernye <lucian.iernye@gmail.com>
2023-12-28 15:34:23 +0000

    add 2 boilerplates for vue 2 using vite

---
+++ Boilerplates/Vue 2/vue-project/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Letter from "@/components/Letter.vue";
+</script>
+
+<template>
+  <div id="app">
+    <h1>Hello, welcome to K-Team Krosswords</h1>
+    <div class="wrapper">
+      <div v-for="item in Array(169)" :key="item">
+        <Letter />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(13, minmax(0, 1fr));
+  grip-template-rows: 50px;
+  gap: 10px;
+}
+
+#app {
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+h1 {
+  text-align: center;
+  margin: 20px;
+}
+
+</style>
---
+++ Boilerplates/Vue 2/vue-project/src/components/Letter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="letter-style">
+    <p>{{ letter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Letter",
+  props: {
+    letter: {
+      type: String,
+      default: 'A'
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+.letter-style {
+  font-size: 16px;
+  border: 1px solid red;
+  padding: 8px 16px;
+}
+</style>
