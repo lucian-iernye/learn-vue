781ed3d133b3c5921f87e25aa007355c03f9e033
Lucian Iernye <lucian.iernye@gmail.com>
2024-01-03 10:36:41 +0000

    init basic vue 3 setup function and add basic variables and function

--- Vue-3/Net-Ninja/dojo-blog/src/views/HomeView.vue
+++ Vue-3/Net-Ninja/dojo-blog/src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home view</h1>
+
+<!--    <pre>Name: {{ name }}</pre>-->
+<!--    <pre>Age: {{ age }}</pre>-->
+
+    <p ref="para">My name is {{ name }} and my age is {{ age }}</p>
+
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
+  },
+  setup() {
+
+    let name = 'mario';
+    let age = 30;
+
+    const handleClick = () => {
+      console.log(para.value)
+
+      para.value = 'new value' // not gonna change. this is only useful to add/change classes
+
+      console.log(para.value)
+    }
+
+    const para = ref(null);
+
+    return { name, age, handleClick, para }
   }
 }
 </script>
